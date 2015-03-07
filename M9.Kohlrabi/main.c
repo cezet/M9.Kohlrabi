e49b2d4ed31652be6500d52dba9fa6201268cdc6
cezet <maciek.czop@gmail.com>
2015-03-07 18:46:44 +0100

    First commit

---
+++ M9.Kohlrabi/main.c
@@ -0,0 +1,17 @@
+/*
+ * Kohlrabi by czoper/MLYNY9
+ *
+ * Created: 07/03/2015 18:41:11
+ *  
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
